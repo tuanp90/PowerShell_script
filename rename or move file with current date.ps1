c3f377015e52ebed93dd200d25743e1cdc228805
tuanp90 <68574104+tuanp90@users.noreply.github.com>
2023-12-01 10:46:22 +0700

    Update and rename rename file witch current date.ps1 to rename or move file with current date.ps1

--- rename or move file with current date.ps1
+++ rename or move file with current date.ps1
@@ -1,7 +1,12 @@
 $currentDate = Get-Date -Format yyyy-MM-dd
 $sourceFile1 = "D:\CiscoBackup\Capture_file\VT_CAPTURE_14h45_KRX.pcap"
 $sourceFile2 = "D:\CiscoBackup\Capture_file\VT_CAPTURE_16h55_KRX.pcap"
+$sourceFile3 = "D:\CiscoBackup\VT_CAPTURE_KRX.pcap"
 $destinationFile1 = "D:\CiscoBackup\Capture_file\VT_CAPTURE_14h45_KRX_$currentDate.pcap"
 $destinationFile2 = "D:\CiscoBackup\Capture_file\VT_CAPTURE_16h55_KRX_$currentDate.pcap"
+$destinationFile3 = "VT_CAPTURE_7h55_KRX_$currentDate.pcap"
+$destinationFolder = "D:\CiscoBackup\Capture_file"
+
 Rename-Item -Path $sourceFile1 -NewName $destinationFile1
 Rename-Item -Path $sourceFile2 -NewName $destinationFile2
+Move-Item -Path $sourceFile3 -Destination "$destinationFolder\$destinationFile3"
